c02f1dbeeb736a75c320801c199e8225c584f49f
Tavo2x <131424278+Tavo2x@users.noreply.github.com>
2026-09-24 13:59:08 -0500

    Driver
    Creation of patient list initializing them as MedicalCondition.
    Created two. Added to list.
    Loops through the list. Showing the treatment of each medical condition in the list.

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+
+        ArrayList <MedicalCondition> patientList = new ArrayList<MedicalCondition>();
+
+        MedicalCondition m1 = new BrokenBone("Gus", "9/23/2026");
+        MedicalCondition m2 = new StrepThroat("Tavo", "9/24/2026");
+
+        patientList.add(m1);
+        patientList.add(m2);
+
+        for (int i =0; i <= patientList.size()-1; i++){
+            System.out.println("Treatment is " + patientList.get(i).getTreatment());
+        } 
     }
 }
